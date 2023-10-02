55f0e1fcb51ac7b6128cb112225acf3988a3ce9a
Merilin P <merilin.pruulrtktk.ee>
2023-10-02 19:41:09 +0300

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//FOR-LOOP
+//DRY-pingiple - Do Not Repeat Yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++) //i++ = i + i
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
+//Console.WriteLine("We don't need no education.");
